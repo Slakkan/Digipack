71a11971e245542a9d16c595b057567cba7d3534
unknown <lnpastinante@gmail.com>
2019-08-06 20:19:51 -0300

    Added Header responsive design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,41 @@
-import * as React from 'react';
+import * as React from "react";
 
-import '../styles/header.css'
+import "../styles/header.css";
 
-export interface HeaderProps {
-    
-}
- 
-const Header: React.SFC<HeaderProps> = () => {
-    return ( 
-        <div className='header'>Digipack</div>
-     );
-}
- 
+export interface HeaderProps {}
+
+const Header: React.SFC<HeaderProps> = props => {
+  return (
+    <div className="header">
+      <div className="title-container">Digipack</div>
+      <div className="link-container">
+        <button
+          className="link"
+          onFocus={e => {
+            e.target.blur();
+          }}
+        >
+          Inicio
+        </button>
+        <button
+          className="link"
+          onFocus={e => {
+            e.target.blur();
+          }}
+        >
+          Envases
+        </button>
+        <button
+          className="link"
+          onFocus={e => {
+            e.target.blur();
+          }}
+        >
+          Contacto
+        </button>
+      </div>
+    </div>
+  );
+};
+
 export default Header;
