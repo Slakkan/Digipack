fae94562cd621b8541101bff01635c7942713245
unknown <lnpastinante@gmail.com>
2019-08-19 17:38:46 -0300

    finally fixed this heroku ****

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,7 +24,7 @@ const Header: React.SFC<HeaderProps> = props => {
             e.target.blur();
           }}
         >
-          <Link className="link" to="/packaging">
+          <Link className="navigation-link" to="/packaging">
             Envases
           </Link>
         </button>
@@ -34,7 +34,7 @@ const Header: React.SFC<HeaderProps> = props => {
             e.target.blur();
           }}
         >
-          <Link className="link" to="/contact">
+          <Link className="navigation-link" to="/contact">
             Contacto
           </Link>
         </button>
