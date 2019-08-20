bbf4120f8ae3cf1f1e5a5f05c6a7d5b80483c597
unknown <lnpastinante@gmail.com>
2019-08-20 18:20:54 -0300

    Added slider

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,57 @@
-import * as React from 'react';
+import * as React from "react";
 
-import Header from '../components/Header'
+import Header from "../components/Header";
+import { Slide } from "react-slideshow-image";
+
+const slideImages = ["/images/slide_1.jpg", "/images/slide_2.jpg", "/images/slide_3.jpg"];
+
+const properties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  arrows: true,
+  onChange: (oldIndex: number, newIndex: number) => {
+    console.log(`slide transition from ${oldIndex} to ${newIndex}`);
+  }
+};
 
 export interface HomeProps {
-    userID: string | undefined
+  userID: string | undefined;
 }
- 
-export interface HomeState {
-}
- 
+
+export interface HomeState {}
+
 class Home extends React.Component<HomeProps, HomeState> {
-    render() { 
-        return ( 
-            <div>
-                <Header />
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="content-container">
+          <Slide {...properties}>
+            <div className="slide-container">
+              <div
+                className="slide"
+                style={{ backgroundImage: `url(${slideImages[0]})` }}
+              />
             </div>
-         );
-    }
+            <div className="slide-container">
+              <div
+                className="slide"
+                style={{ backgroundImage: `url(${slideImages[1]})` }}
+              />
+            </div>
+            <div className="slide-container">
+              <div
+                className="slide"
+                style={{ backgroundImage: `url(${slideImages[2]})` }}
+              />
+            </div>
+          </Slide>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Home;
