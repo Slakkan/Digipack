64aae6d41eaa79f2f9cfe441b4e7e49270fc5450
unknown <lnpastinante@gmail.com>
2019-08-27 14:47:11 -0300

    still fixing heroku

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,6 @@ module.exports = env => {
     if (process.env.NODE_ENV === 'development') {
         var dotenv = require('dotenv').config({ path: __dirname + '/.env' });
         process.env = dotenv.parsed
-        console.log('patatas')
     }
     
     return {
@@ -48,16 +47,12 @@ module.exports = env => {
                 ignoreOrder: false,
             }),
             new webpack.DefinePlugin({
-                process: {
-                    env: {
-                        FIREBASE_API_KEY: JSON.stringify(process.env.FIREBASE_API_KEY),
-                        FIREBASE_AUTH_DOMAIN: JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-                        FIREBASE_DATABASE_URL: JSON.stringify(process.env.FIREBASE_DATABASE_URL),
-                        FIREBASE_PROJECT_ID: JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-                        FIREBASE_STORAGE_BUCKET: JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
-                        FIREBASE_MESSAGING_SENDER_ID: JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
-                    }
-                }
+                'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+                'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+                'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+                'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+                'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+                'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
             })
         ],
         devServer: {
